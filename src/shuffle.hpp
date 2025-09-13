c91685a0322f043e31429dd0c14676420d34b8ac
Your Name <you@example.com>
2025-09-13 11:56:34 -0500

    Remove algorithm comments from header file

--- src/shuffle.hpp
+++ src/shuffle.hpp
@@ -4,12 +4,9 @@
 #include <random>
 #include <algorithm>
 
-// Algorithm A: Random sort (biased - worst)
 void shuffle_random_sort(std::vector<int>& array);
 
-// Algorithm B: Naive swap (biased - middle)  
 void shuffle_naive_swap(std::vector<int>& array);
 
-// Algorithm C: Fisher-Yates (unbiased - best)
 void shuffle_fisher_yates(std::vector<int>& array);
 
